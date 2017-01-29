7acd3e32d34babff92a567e9282e0a656ec53fcb
Vectri <ctrlvectri@gmail.com>
2017-01-29 15:20:57 -0800

    Initial commit

---
+++ src/main/java/io/github/vectri/tpa/TPA.java
@@ -0,0 +1,19 @@
+package io.github.vectri.tpa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *  The main file for TPA.
+ */
+public final class TPA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
