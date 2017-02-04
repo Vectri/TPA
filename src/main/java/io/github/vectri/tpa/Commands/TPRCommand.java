08b1a9b1d1170f3f3441d93adacf3d54a8621437
Vectri <ctrlvectri@gmail.com>
2017-02-03 22:38:18 -0800

    Update TPRCommand to correspond with Request updates

--- src/main/java/io/github/vectri/tpa/Commands/TPRCommand.java
+++ src/main/java/io/github/vectri/tpa/Commands/TPRCommand.java
@@ -1,17 +1,28 @@
 package io.github.vectri.tpa.Commands;
 
+import io.github.vectri.tpa.Requests.RequestHandler;
+import io.github.vectri.tpa.TPA;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.UUID;
 
 
 /**
  * Handles the requesting of teleports.
  */
 public class TPRCommand implements CommandExecutor {
+    private TPA plugin;
+    private static RequestHandler requestHandler;
+
+    public TPRCommand(TPA plugin) {
+        this.plugin = plugin;
+        requestHandler = new RequestHandler(plugin);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
@@ -19,7 +30,15 @@ public class TPRCommand implements CommandExecutor {
             if (target != null) {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     if (player.getName().equalsIgnoreCase(target)) {
-                        sender.sendMessage("Found your player!");
+                        UUID playerUniqueId = player.getUniqueId();
+                        UUID senderUniqueId = ((Player) sender).getUniqueId();
+                        if (requestHandler.alreadyRequested(playerUniqueId, senderUniqueId)) {
+                            sender.sendMessage("You have already requested to teleport to " + player.getName() + "!");
+                        } else {
+                            requestHandler.add(playerUniqueId, senderUniqueId);
+                            sender.sendMessage("Sent a teleport request to " + player.getName() + ".");
+                            player.sendMessage(sender.getName() + " has requested to teleport to you. /tpa " + sender.getName() + " to accept.");
+                        }
                         return true;
                     }
                 }
