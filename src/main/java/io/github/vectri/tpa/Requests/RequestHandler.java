d2e5c2020dae7c1a73b8f3d1a2dcfb02d12d5a47
Vectri <ctrlvectri@gmail.com>
2017-02-03 22:27:18 -0800

    Refactor RequestHandler to be more concise

--- src/main/java/io/github/vectri/tpa/Requests/RequestHandler.java
+++ src/main/java/io/github/vectri/tpa/Requests/RequestHandler.java
@@ -1,7 +1,6 @@
 package io.github.vectri.tpa.Requests;
 
 import io.github.vectri.tpa.TPA;
-import org.bukkit.entity.Player;
 
 import java.util.*;
 
@@ -10,65 +9,49 @@ import java.util.*;
  */
 public class RequestHandler {
     private TPA plugin;
-    private static Map<Player, ArrayList<Player>> teleportRequests = new TreeMap<>(new PlayerComparator());
+    private static ArrayList<Request> requestArrayList = new ArrayList<>();
 
     public RequestHandler(TPA plugin) {
         this.plugin = plugin;
     }
 
-    public void addRequest(Player target, Player requester) {
-        if (!teleportRequests.containsKey(target)) {
-            teleportRequests.put(target, new ArrayList<>());
-        }
-        if (teleportRequests.get(target) != null) {
-            plugin.getLogger().info("added");
-            teleportRequests.get(target).add(requester);
-        }
-        new RequestTimer(plugin, this, target, requester);
-    }
-
-    void removeRequest(Player target, Player requester) {
-        ArrayList requests = teleportRequests.get(target);
-        if (requests.contains(requester)) {
-            requests.remove(requester);
-        }
-        if (requests.isEmpty()) {
-            teleportRequests.remove(target);
-        }
-    }
-
-    public ArrayList<Player> getRequests(Player target) {
-        if (teleportRequests.containsKey(target)) {
-            return teleportRequests.get(target);
+    private Request get(UUID target) {
+        for (Request request : requestArrayList) {
+            if (request.getTarget() == target) {
+                return request;
+            }
         }
         return null;
     }
 
-    public boolean hasRequested(Player requester) {
-        for (ArrayList<Player> requests : teleportRequests.values()) {
-            if (requests.contains(requester)) {
-                return true;
-            }
+    public void add(UUID target, UUID requester) {
+        Request request = get(target);
+        if (request == null) {
+            requestArrayList.add(new Request(target, requester));
+            new RequestTimer(plugin, this, target, requester);
+            return;
+        }
+        ArrayList<UUID> requesterList = request.getRequesterList();
+        requesterList.add(requester);
+    }
+
+    boolean remove(UUID target, UUID requester) {
+        Request request = get(target);
+        if (request != null) {
+            requestArrayList.remove(request);
+            return true;
         }
         return false;
     }
 
-    public boolean inRequests(Player target) {
-        for (Player player : teleportRequests.keySet()) {
-            if (player == target) {
+    public boolean alreadyRequested(UUID target, UUID requester) {
+        Request request = get(target);
+        if (request != null) {
+            ArrayList<UUID> requesterList = request.getRequesterList();
+            if (requesterList.contains(requester)) {
                 return true;
             }
         }
         return false;
-    }
-
-    private static class PlayerComparator implements Comparator<Player> {
-        @Override
-        public int compare(Player o1, Player o2) {
-            if (o1.getUniqueId() == o2.getUniqueId()) {
-                return 1;
-            }
-            return 0;
-        }
     }
 }
