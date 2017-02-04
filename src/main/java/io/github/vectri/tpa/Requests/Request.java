e679be11bef7dfc52d8d692ffc9258b8053ecb27
Vectri <ctrlvectri@gmail.com>
2017-02-03 22:19:25 -0800

    Add Request object

---
+++ src/main/java/io/github/vectri/tpa/Requests/Request.java
@@ -0,0 +1,25 @@
+package io.github.vectri.tpa.Requests;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Represents a request object.
+ */
+class Request {
+    private UUID target;
+    private ArrayList<UUID> requesterList = new ArrayList<>();
+
+    Request(UUID target, UUID requester) {
+        this.target = target;
+        requesterList.add(requester);
+    }
+
+    UUID getTarget() {
+        return target;
+    }
+
+    ArrayList<UUID> getRequesterList() {
+        return requesterList;
+    }
+}
